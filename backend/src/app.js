dd022d582db46a5886fb3e8ccfa044a75f189bff
Radhika Patidar <radspatidar16@gmail.com>
2026-09-23 13:47:19 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5174",
   "http://127.0.0.1:5175",
   "https://dms-one.netlify.app/",
-  "https://dms-one.netlify.app"
+  "https://dms-one.netlify.app",
+  "http://dms-frontend-lb-179175988.ap-south-1.elb.amazonaws.com/"
 ];
 
 const corsOptions = {
